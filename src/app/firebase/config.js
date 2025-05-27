036220ae2bb483b67ad46e7b831bb2ab154bfe14
yoyodragon <ved.bankeshwar@gmail.com>
2025-05-27 23:49:48 +0530

    fix authentication by creating .env

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCj01-T9BihQTFNrKlRKs4mT2GyCS6Nvh0",
-  authDomain: "battlecode-dcb79.firebaseapp.com",
-  projectId: "battlecode-dcb79",
-  storageBucket: "battlecode-dcb79.firebasestorage.app",
-  messagingSenderId: "319352396507",
-  appId: "1:319352396507:web:833b43d083167c47894afe"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!
 };
 
 const app = initializeApp(firebaseConfig);
