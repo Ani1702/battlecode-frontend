5ded51558214486ff60a607d99aa6d116984ac9d
Chandramouli Subramanian <chandramoulisubramanian@Chandramoulis-MacBook-Pro.local>
2025-06-18 16:04:54 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="font-orbitron flex flex-col items-center justify-center h-screen bg-black text-white">
+      <h1 className="text-9xl ">BATTLECODE</h1>
+    </div>
+  );
+}
